7ca2755f5607a124d124f65997a1369bc450b058
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-05-13 12:51:36 +0100

    Using State

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = 'bond';
+    const [name, setName] = useState('bond');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
+    const handleClick = () => {
+        setName('james');
+        setAge(30);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('bond', e)}>Click me again</button>
         </div>
      );
 }
